9da0077caa97e248be0e2be007b259daf361b139
IamAgbaCoder <demiladebamgboye@gmail.com>
2025-11-28 18:38:42 +0100

    Update code

--- js/main.js
+++ js/main.js
@@ -138,7 +138,7 @@
 
     // Typing Effect
     const typingTextElements = document.querySelectorAll(".typing-text");
-    const phrases = ["We're getting married", "#AnEndlessOcean'25", "November 29th, 2025"];
+    const phrases = ["We're getting married", "#AnEndlessLove'25", "November 29th, 2025"];
     let phraseIndex = 0;
     let charIndex = 0;
     let isDeleting = false;
